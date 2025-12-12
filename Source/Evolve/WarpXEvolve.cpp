04a5662ddff247713bd1cf164a9cbb5e7b31ff02
Justin Angus <angus1@llnl.gov>
2025-12-12 13:30:48 -0800

    remove blank line.

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -396,7 +396,6 @@ void WarpX::OneStep (
 
     // implicit solver
     if (m_implicit_solver) {
-
         // advance fields and particles by one time step
         m_implicit_solver->OneStep(a_cur_time, a_dt, a_step);
     }
