a14025c7881988edd0071a0ad1077d315ab30ad5
Justin Angus <angus1@llnl.gov>
2026-01-16 09:08:29 -0800

    fix 1D compile warnings.

--- Source/FieldSolver/ImplicitSolvers/ImplicitSolver.cpp
+++ Source/FieldSolver/ImplicitSolvers/ImplicitSolver.cpp
@@ -1042,10 +1042,12 @@ void ImplicitSolver::FinishMassMatrices ()
 #endif
     }
 
+#if AMREX_SPACEDIM == 2
     const amrex::IntVect ncomp_xx = m_ncomp_xx;
     const amrex::IntVect ncomp_zz = m_ncomp_zz;
 #if !defined(WARPX_EM_TEY)
     const amrex::IntVect ncomp_yy = m_ncomp_yy;
+#endif
 #endif
 
     for (int lev = 0; lev < m_num_amr_levels; ++lev) {
