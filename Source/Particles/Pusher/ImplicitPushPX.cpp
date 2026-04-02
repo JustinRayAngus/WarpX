d3dce33b074dbe692a7e6517f32242166056f768
Justin Angus <angus1@llnl.gov>
2026-04-01 19:01:37 -0700

    rotate particles back to zero angle at end of push.

--- Source/Particles/Pusher/ImplicitPushPX.cpp
+++ Source/Particles/Pusher/ImplicitPushPX.cpp
@@ -1192,6 +1192,18 @@ PhysicalParticleContainer::ImplicitPushXPSubOrbits (WarpXParIter& pti,
                 uxp_n = 2.0_prt*ux[ip] - uxp_n;
                 uyp_n = 2.0_prt*uy[ip] - uyp_n;
                 uzp_n = 2.0_prt*uz[ip] - uzp_n;
+#if defined(WARPX_DIM_RCYLINDER) || defined(WARPX_DIM_RZ)
+                // rotate particles back to theta = 0.
+                const amrex::ParticleReal rp = std::sqrt(xp_n*xp_n + yp_n*yp_n);
+                const amrex::ParticleReal costh = (rp > 0. ? xp_n/rp : 1.0_prt);
+                const amrex::ParticleReal sinth = (rp > 0. ? yp_n/rp : 0.0_prt);
+                const amrex::ParticleReal upx = uxp_n;
+                const amrex::ParticleReal upy = uyp_n;
+                uxp_n =  costh*upx + sinth*upy;
+                uyp_n = -sinth*upx + costh*upy;
+                xp_n = rp;
+                yp_n = 0.0;
+#endif
                 ux[ip] = uxp_n;
                 uy[ip] = uyp_n;
                 uz[ip] = uzp_n;
